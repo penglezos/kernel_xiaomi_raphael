17fccd9ab95b7e5781fa0921be7f8e8bf5acca3e
penglezos <panagiotisegl@gmail.com>
2020-07-01 16:43:23 +0300

    Revert "cpufreq: schedutil: Expose default rate-limits as config options"
    This reverts commit c3f69adc74ca59bfa8a12bc9fbdc73a3236668ef.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -774,9 +774,9 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 
 	tunables->up_rate_limit_us =
-				CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
+				cpufreq_policy_transition_delay_us(policy);
 	tunables->down_rate_limit_us =
-				CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
+				cpufreq_policy_transition_delay_us(policy);
 
 	tunables->iowait_boost_enable = true;
 	policy->governor_data = sg_policy;
