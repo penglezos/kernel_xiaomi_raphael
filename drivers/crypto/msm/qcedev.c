dc9233d63bfa97380e8d64a0b452e55de13f332d
penglezos <panagiotisegl@gmail.com>
2020-07-30 20:43:31 +0300

    drivers: crypto: msm: qcedev: fix misleading indentation warnings

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -1802,10 +1802,11 @@ static inline long qcedev_ioctl(struct file *file,
 				&handle->sha_ctxt.digest[0],
 				handle->sha_ctxt.diglen);
 		mutex_unlock(&hash_access_lock);
-		if (copy_to_user((void __user *)arg, &qcedev_areq->sha_op_req,
-					sizeof(struct qcedev_sha_op_req)))
+		if (copy_to_user((void __user *)arg, &qcedev_areq->sha_op_req, 
+					sizeof(struct qcedev_sha_op_req))) {
 			err = -EFAULT;
 			goto exit_free_qcedev_areq;
+			}
 		}
 		break;
 
@@ -1895,9 +1896,10 @@ static inline long qcedev_ioctl(struct file *file,
 				handle->sha_ctxt.diglen);
 		mutex_unlock(&hash_access_lock);
 		if (copy_to_user((void __user *)arg, &qcedev_areq->sha_op_req,
-					sizeof(struct qcedev_sha_op_req)))
+					sizeof(struct qcedev_sha_op_req))) {
 			err = -EFAULT;
 			goto exit_free_qcedev_areq;
+			}
 		}
 		break;
 
